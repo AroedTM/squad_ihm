0e02f93965680e7a1c9fb4874eeac98ddd31d4b1
Maxence Fortin <fortin@et.esiea.fr>
2021-12-07 15:51:34 +0100

    Scoreboard fix ratio decimals

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ serveur.listen(8080, function(){});
 app.get('/scoreboard', function (req, res) {
     let data = {parties: [], victoires: 0, defaites: 0, ratioEvolution: []};
     data.parties = [
+        {id: 5, result: "Victoire", pointsPlayer1: 10, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
         {id: 4, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
         {id: 3, result: "Victoire", pointsPlayer1: 5, pointsPlayer2: 2, duree: 500, date: "07/12/2021"},
-        {id: 2, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
-        {id: 1, result: "Victoire", pointsPlayer1: 3, pointsPlayer2: 0, duree: 100, date: "02/12/2021"},
-        {id: 0, result: "Victoire", pointsPlayer1: 90, pointsPlayer2: 2, duree: 3000, date: "01/12/2021"},
+        {id: 2, result: "Victoire", pointsPlayer1: 12, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
+        {id: 1, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
+        
     ];
     for(let i=0; i<data.parties.length; i++)
     {
