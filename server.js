c266259b77a9409df0df6f08a8895e08b1433226
Maxime Maitrot <maitrot@et.esiea.fr>
2021-12-08 09:00:55 +0100

    Mise a jour des redirections

--- server.js
+++ server.js
@@ -2,31 +2,46 @@ var express = require('express');
 var serve_static = require('serve-static');
 var http = require('http');
 var fs = require('fs');
-
+var path = require('path');
 var app = express();
-app.use(serve_static(__dirname+"/public"));
+app.use(serve_static(__dirname + "/public"));
 
 var serveur = http.Server(app);
-serveur.listen(8080, function(){});
-
+serveur.listen(8080, function () { });
+app.get('/game', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/jeu.html'));
+});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/auth.html'));
+});
+app.get('/auth', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/auth.html'));
+});
+app.get('/accueil', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/Accueil.html'));
+});
+app.get('/leaderboard', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/scoreboard.html'));
+});
 app.get('/scoreboard', function (req, res) {
-    let data = {parties: [], victoires: 0, defaites: 0};
+    let data = { parties: [], victoires: 0, defaites: 0, ratioEvolution: [] };
     data.parties = [
-        {id: 4, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
-        {id: 3, result: "Victoire", pointsPlayer1: 5, pointsPlayer2: 2, duree: 500, date: "07/12/2021"},
-        {id: 2, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021"},
-        {id: 1, result: "Victoire", pointsPlayer1: 3, pointsPlayer2: 0, duree: 100, date: "02/12/2021"},
-        {id: 0, result: "Victoire", pointsPlayer1: 90, pointsPlayer2: 2, duree: 3000, date: "01/12/2021"},
+        { id: 5, result: "Victoire", pointsPlayer1: 10, pointsPlayer2: 2, duree: 200, date: "02/12/2021" },
+        { id: 4, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021" },
+        { id: 3, result: "Victoire", pointsPlayer1: 5, pointsPlayer2: 2, duree: 500, date: "07/12/2021" },
+        { id: 2, result: "Victoire", pointsPlayer1: 12, pointsPlayer2: 2, duree: 200, date: "02/12/2021" },
+        { id: 1, result: "Defaite", pointsPlayer1: 1, pointsPlayer2: 2, duree: 200, date: "02/12/2021" },
+
     ];
-    for(let i=0; i<data.parties.length; i++)
-    {
-        if(data.parties[i].result == "Victoire")
+    for (let i = 0; i < data.parties.length; i++) {
+        if (data.parties[i].result == "Victoire")
             data.victoires++;
         else
             data.defaites++;
     }
+
+    for (let i = 0; i < data.parties.length; i++) {
+        data.ratioEvolution.push(data.parties[i].pointsPlayer1 / data.parties[i].pointsPlayer2);
+    }
     res.send(data);
 });
-
-
-   
